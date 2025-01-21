d0fe8415c98123d4461bbac30d1844e92b181947
dewanshkaushik <kaushik.dewansh1@gmail.com>
2025-01-22 00:29:55 +0530

    commit by dewansh (readm me included)

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.todoapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -21,6 +22,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FabPosition
@@ -34,43 +36,72 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.todoapp.database.DatabaseClient
 import com.example.todoapp.dialog.AlertDialog
+import com.example.todoapp.model.TodoItem
 import com.example.todoapp.ui.theme.TodoAppTheme
-import com.example.todoapp.view.EnterNewItemActivity
 import com.example.todoapp.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val mainViewModel: MainViewModel by viewModels()
+    private val sharedViewModel: MainViewModel by viewModels()
+    lateinit var navController: NavHostController;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TodoAppTheme {
-                FloatingBar()
-            }
+                navController = rememberNavController()
 
+                NavHost(navController, startDestination = "firstScreen") {
+                    composable("firstScreen") {
+                        FloatingBar(navController, sharedViewModel)
+                    }
+                    composable("secondScreen") { backStackEntry ->
+                        val message = backStackEntry.arguments?.getString("message") ?: "No message"
+                        AddToDO(navController, sharedViewModel)
+                    }
+                }
+
+                //d   FloatingBar()
+            }
         }
     }
 
     override fun onResume() {
         super.onResume()
-        mainViewModel.getData()
+        sharedViewModel.getData()
     }
 
     private fun onClick() {
-        gotoNextActivity()
+        //  gotoNextActivity()
+        navController.navigate("secondScreen")
+
     }
 
     @Composable
@@ -97,8 +128,17 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun FloatingBar() {
-
+    fun FloatingBar(navController: NavController, sharedViewModel: MainViewModel) {
+        val result = rememberSaveable { mutableStateOf<String?>(null) }
+        val context = LocalContext.current
+        // Listen for the result
+        LaunchedEffect(result) {
+            navController.currentBackStackEntry?.savedStateHandle?.getLiveData<String>("resultKey")
+                ?.observeForever { newResult ->
+                    result.value = newResult;
+                    Toast.makeText(context, "Failed to add TODO", Toast.LENGTH_SHORT).show()
+                }
+        }
         Scaffold(topBar = { },
             floatingActionButtonPosition = FabPosition.End,
             floatingActionButton = {
@@ -111,49 +151,18 @@ class MainActivity : ComponentActivity() {
                 }
             }, content = { padding ->
                 Surface(modifier = Modifier.padding(padding)) {
-                    List(mainViewModel)
+                    List(sharedViewModel)
                 }
             })
     }
 
-    companion object {
-        const val START_ACTIVITY_3_REQUEST_CODE = 0
-    }
-
-    private fun gotoNextActivity() {
-        val intent = Intent(this, EnterNewItemActivity::class.java)
-        startActivityForResult(intent, START_ACTIVITY_3_REQUEST_CODE)
-    }
-
-    @Deprecated("This method has been deprecated in favor of using the Activity Result API\n      which brings increased type safety via an {@link ActivityResultContract} and the prebuilt\n      contracts for common intents available in\n      {@link androidx.activity.result.contract.ActivityResultContracts}, provides hooks for\n      testing, and allow receiving results in separate, testable classes independent from your\n      activity. Use\n      {@link #registerForActivityResult(ActivityResultContract, ActivityResultCallback)}\n      with the appropriate {@link ActivityResultContract} and handling the result in the\n      {@link ActivityResultCallback#onActivityResult(Object) callback}.")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == START_ACTIVITY_3_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                val message = data!!.getStringExtra("message")
-                if (message != null) {
-                    if (message.trim() == "error") {
-                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-                        setContent {
-                            val openAlertDialog = rememberSaveable { mutableStateOf(true) }
-                            ShowDialog("Failed to add TODO", openAlertDialog)
-                            FloatingBar()
-                        }
-                    }
-                }
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
-    }
-
     @Composable
     fun List(mainViewModel: MainViewModel) {
-        //search
         //list
         val posts by rememberSaveable {
             mainViewModel.todolist
         }
-// State to hold the search query
+        // State to hold the search query
         var searchQuery by rememberSaveable { mutableStateOf("") }
 
         // Filter items based on the search query
@@ -236,6 +245,66 @@ class MainActivity : ComponentActivity() {
                 dialogText = data,
                 icon = Icons.Default.Info,
             )
+        }
+    }
+
+    // new screen
+    @Composable
+    fun AddToDO(
+        navController: NavController,
+        sharedViewModel: MainViewModel
+    ) {
+        var text by rememberSaveable { mutableStateOf("") }
+        Column {
+            Text(
+                text = "Add To Do!",
+                modifier = Modifier.padding(2.dp)
+            )
+
+            TextField(
+                value = text,
+                onValueChange = { newText -> text = newText },
+                label = { Text("Enter text") },
+                placeholder = { Text("Type here...") },
+                shape = MaterialTheme.shapes.medium, // Rounded corners
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Button(
+                onClick = { onclick(text) },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Add Todo")
+            }
+        }
+    }
+
+
+    private fun onclick(text: String) {
+        val user = TodoItem()
+        if (!text.trim().equals("Error", true)) {
+            user.itemName = text
+            lifecycleScope.launch {
+                val async = lifecycleScope.async(Dispatchers.IO) {
+                    DatabaseClient.getInstance(applicationContext)?.getAppDatabase()
+                        ?.itemDao()
+                        ?.insertItem(user)
+                }
+                async.await()
+                setContent {
+                    IndeterminateCircularIndicator()
+                    Handler().postDelayed({
+                        setContent {
+                            FloatingBar(navController = navController, sharedViewModel = sharedViewModel)
+                        }
+                    },3000)
+                }
+            }
+        } else {
+            Toast.makeText(this, "Failed to add TODO", Toast.LENGTH_SHORT).show()
+            navController.previousBackStackEntry?.savedStateHandle?.set("resultKey", "error")
+            setContent {
+                FloatingBar(navController = navController, sharedViewModel = sharedViewModel)
+            }
         }
     }
 }
